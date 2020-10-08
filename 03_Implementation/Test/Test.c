12c321e30d0f75814b7307b3650ca2b3daab7d12
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-09 02:58:51 +0530

    Update Test.c

--- 03_Implementation/Test/Test.c
+++ 03_Implementation/Test/Test.c
@@ -12,7 +12,7 @@ int main() {
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-  CU_add_test(suite, "login", testlogin);
+  CU_add_test(suite, "login", test_login);
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
 
