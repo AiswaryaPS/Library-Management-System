3aa725310ef144c65aa149dd5773294517a700cc
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-09 01:22:51 +0530

    Update Library.h

--- 03_Implementation/inc/Library.h
+++ 03_Implementation/inc/Library.h
@@ -1,26 +1,24 @@
 #ifndef LIBRARY_H_INCLUDED
 #define LIBRARY_H_INCLUDED
-//#include<windows.h>
+#include<windows.h>
 #include<stdio.h>
 //#include<conio.h>
 #include <stdlib.h>
-//#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
-//#include<ctype.h>                   //contains toupper(), tolower(),etc
-//#include<dos.h>                     //contains _dos_getdate
-//#include<time.h>
+#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
+#include<ctype.h>                   //contains toupper(), tolower(),etc
 int login();
-void returnfunc(void);
-void mainmenu(void);
-void addbooks(void);
-void deletebooks(void);
-void editbooks(void);
-void searchbooks(void);
-void issuebooks(void);
-void viewbooks(void);
-void closeapplication(void);
-void Password();
-void issuerecord();
-void loaderanim();
+//void returnfunc(void);
+//void mainmenu(void);
+//void addbooks(void);
+//void deletebooks(void);
+//void editbooks(void);
+//void searchbooks(void);
+//void issuebooks(void);
+//void viewbooks(void);
+//void closeapplication(void);
+//void Password();
+//void issuerecord();
+//void loaderanim();
 
 #endif // LIBRARY_H_INCLUDED
 
