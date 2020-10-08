1b658f7ef5786760188aadedc4654b3d46f9f639
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-09 01:11:41 +0530

    Update Library.h

--- 03_Implementation/inc/Library.h
+++ 03_Implementation/inc/Library.h
@@ -1,13 +1,13 @@
 #ifndef LIBRARY_H_INCLUDED
 #define LIBRARY_H_INCLUDED
-#include<windows.h>
+//#include<windows.h>
 #include<stdio.h>
-#include<conio.h>
+//#include<conio.h>
 #include <stdlib.h>
-#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
-#include<ctype.h>                   //contains toupper(), tolower(),etc
-#include<dos.h>                     //contains _dos_getdate
-#include<time.h>
+//#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
+//#include<ctype.h>                   //contains toupper(), tolower(),etc
+//#include<dos.h>                     //contains _dos_getdate
+//#include<time.h>
 
 void returnfunc(void);
 void mainmenu(void);
@@ -18,9 +18,6 @@ void searchbooks(void);
 void issuebooks(void);
 void viewbooks(void);
 void closeapplication(void);
-int  getdata();
-int  checkid(int);
-int t(void);
 void Password();
 void issuerecord();
 void loaderanim();
