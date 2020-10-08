6b11ca176c711992bf22da2b492da029fcff828c
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-09 02:35:10 +0530

    Update library.c

--- 03_Implementation/src/library.c
+++ 03_Implementation/src/library.c
@@ -1,15 +1,15 @@
 #include<Library.h>
 //void gotoxy (int x, int y)
-{
+//{
 //coord.X = x; coord.Y = y; // X and Y coordinates
 //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
+//}
 
 char password[10]={"pass"};
 void main()
 {
  int k;
-k=Password(password);
+k=password(password);
 if(k==1)
 {
 mainmenu();
@@ -18,7 +18,6 @@ else
 {
 printf("login unsuccessfull");
 main();
-
 }
 }
 getch();
