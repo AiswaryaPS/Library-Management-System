2d76e82c30cc2818c2c51583e6669b652e620ab1
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-09 04:34:54 +0530

    Update Test.c

--- 03_Implementation/Test/Test.c
+++ 03_Implementation/Test/Test.c
@@ -9,25 +9,25 @@
 
 //int main() {
 
-//  if (CUE_SUCCESS != CU_initialize_registry())
-  //  return CU_get_error();
-  //CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-  //CU_add_test(suite, "login", test_login);
+ //if (CUE_SUCCESS != CU_initialize_registry())
+   // return CU_get_error();
+ // CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+ // CU_add_test(suite, "login", test_login);
 
-  //CU_basic_set_mode(CU_BRM_VERBOSE);
-
-
-  //CU_basic_run_tests();
+ // CU_basic_set_mode(CU_BRM_VERBOSE);
 
 
-  //CU_cleanup_registry();
+ // CU_basic_run_tests();
 
-  //return 0;
+
+ // CU_cleanup_registry();
+
+ // return 0;
 //}
 
 //void test_login(void) {
 //  CU_ASSERT(1 == login("pass"));
-//  CU_ASSERT(1 == login("wrong"));//dummy fail case
+ // CU_ASSERT(1 == login("wrong"));//dummy fail case
 //}
 
 
