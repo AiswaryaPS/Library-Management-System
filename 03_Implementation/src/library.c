5bad54efa211bc20d4d4cf8f42b0e0c9f5a5712f
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-09 04:19:59 +0530

    Update library.c

--- 03_Implementation/src/library.c
+++ 03_Implementation/src/library.c
@@ -4,6 +4,12 @@
 //coord.X = x; coord.Y = y; // X and Y coordinates
 //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 //}
+struct books
+{
+int id;
+char stname[20];
+char name[20];
+char Author[20];
 
 char password[10]={"pass"};
 void main()
