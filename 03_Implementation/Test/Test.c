87c5bdfd1b6bdc82b5cb93fab59feb098f006f1c
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-08 21:54:05 +0530

    Update Test.c

--- 03_Implementation/Test/Test.c
+++ 03_Implementation/Test/Test.c
@@ -1,7 +1,7 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 
-#define PROJECT_NAME    "Library Management"
+#define PROJECT_NAME    "Library Management System"
 #include<Library.h>
 
 void test_login(void);
@@ -13,4 +13,23 @@ int main() {
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
   CU_add_test(suite, "login", testlogin);
-;
+
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+
+  CU_basic_run_tests();
+
+
+  CU_cleanup_registry();
+
+  return 0;
+}
+
+void test_login(void) {
+  CU_ASSERT(1 == login("pass"));
+  CU_ASSERT(1 == login("wrong"));//dummy fail case
+
+
+
+}
+
