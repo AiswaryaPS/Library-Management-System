ffd37055c4305abe759a7107ab4d7f8283240e85
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-12 22:04:08 +0530

    Update library.c

--- 03_Implementation/src/library.c
+++ 03_Implementation/src/library.c
@@ -1,21 +1,56 @@
 #include<Library.h>
-//void gotoxy (int x, int y)
-//{
-//coord.X = x; coord.Y = y; // X and Y coordinates
-//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-//}
+int Password(char password[10]) //for password option
+{
 
-char Password[10]={"pass"};
-void main()
+system("cls");
+//char d[25]="Password Protected";
+char pass[10];
+//textbackground(WHITE);
+//textcolor(RED);
+//gotoxy(10,4);
+/*for(j=0;j<20;j++)
 {
- int k;
-//k=password(password);
-if(k==1)
+//Sleep(50);
+printf("*");
+}
+for(j=0;j<20;j++)
 {
-printf("login unsuccessfull");
-main();
+//Sleep(50);
+printf("%c",d[j]);
+}
+for(j=0;j<20;j++)
+{
+//Sleep(50);
+printf("*");
+}
+//gotoxy(10,10);
+//gotoxy(15,7);
+printf("Enter Password:");
+
+while(ch!=13)
+{
+ int d;
+ scanf("%d",&d);
+ch=d;
+
+if(ch!=13 && ch!=8){
+//putc('*');
+//pass[i] = ch;
+//i++;
 }
 }
+pass[i] = '\0';*/
+printf("Enter Password:");
+scanf("%s",Password);
+if(strcmp(pass,password)==0)
+{
+
+//gotoxy(15,9);
+//textcolor(BLINK);
+printf("Password match");
+//gotoxy(17,10);
+printf("Press enter to countinue.....");
+scanf("%c",&l);
 //getch();
 
 
